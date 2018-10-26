5608433e08ad1b8195365efec3a1beabe89121bc
taylor <mhy845879017@gmail.com>
2018-10-26 19:15:00 +0800

    Taylor Mei

--- mirror.py
+++ mirror.py
@@ -31,7 +31,8 @@ class MirrorConfig(Config):
     IMAGE_MAX_DIM = 640
 
     BACKBONE = "resnet101"
-    Pretrained_Model_Path = "/home/mhy/Mirror-Segmentation/pspnet101_voc2012.h5"
+    # Pretrained_Model_Path = "/home/mhy/Mirror-Segmentation/pspnet101_voc2012.h5"
+    Pretrained_Model_Path = "/root/pspnet101_voc2012.h5"
 
     BACKBONE_STRIDES = [4, 8, 16, 32, 64]   # for compute pyramid feature size
 
